7fc032752bc89e5813b3c400f057dfdc2243f1f7
Christian Moscardi <clm2183@columbia.edu>
2012-01-30 14:16:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetterCourseworks::Application.config.secret_token = 'ad92765144f50b14f7a41f94d63766746f80567c639d733ac0f16985320a16644f6b6fa20da684a73beb6900b170ef188f6de0c2facfb15196a6684034bbf81f'
